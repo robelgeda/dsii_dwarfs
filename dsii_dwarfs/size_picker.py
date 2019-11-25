c440719db7dc008c0a00e5a00177f809d91d5903
Robel Geda <rgeda@stsci.edu>
2019-11-25 18:44:18 -0500

    size_picker.py

--- dsii_dwarfs/size_picker.py
+++ dsii_dwarfs/size_picker.py
@@ -1,4 +1,3 @@
-import numpy as np
 from scipy import stats
 
 class SizePicker:
@@ -7,13 +6,13 @@ class SizePicker:
     (doi:10.1111/j.1365-2966.2011.18669.x), and they provide a table of their input data. Fit a
     linear relation of logRe vs logM* to that and measured the scatter. We use a t distribution
     with 5 degrees of freedom for the scatter to give it more outliers than a Gaussian.
- 
     """
     def __init__(self):
         self.c0 = 1.74038461
         self.c1 = 0.13943115
         self.sigma = 0.17
         self.scatter = stats.t(loc=0,scale=self.sigma,df=5)
-    def size(self,logmass):
+
+    def size(self, log_mass):
         """ Given a mass, return a random draw from the size-mass relation """
-        return self.c1*logmass+self.c0 + self.scatter.rvs(len(logmass))
+        return self.c1*log_mass+self.c0 + self.scatter.rvs(len(log_mass))
