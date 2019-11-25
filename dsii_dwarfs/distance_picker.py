261ad71d9849dc66ef7caeb3ddcf0dd0050e6631
Robel Geda <rgeda@stsci.edu>
2019-11-25 17:41:33 -0500

    distance_picker.py

--- dsii_dwarfs/distance_picker.py
+++ dsii_dwarfs/distance_picker.py
@@ -4,18 +4,24 @@ class DistancePicker:
     """
     Select a random distance for population with a uniform density
     """
-    def __init__(self,dmax):
+    def __init__(self, dmax, dmin=0.):
         """ 
-        Initialise: Set the maximum distance 
+        Initialise: Set the minimum and maximum distance in MPc
         """
+        self.dmin = dmin
+        self.dmin2 = dmin**2
+
         self.dmax = dmax
-        self.dmax2 = dmax**2
+        self.dmax2 = dmax ** 2
+
     def pick_distance(self):
-        x = np.random.uniform(0,self.dmax)
-        y = np.random.uniform(0,self.dmax)
-        z = np.random.uniform(0,self.dmax)
-        r2 = x**2 + y**2 + z**2
+        r2 = None
+
+        while r2 is None or r2 > self.dmax2 or r2 < self.dmin:
+            x = np.random.uniform(self.dmin, self.dmax)
+            y = np.random.uniform(self.dmin, self.dmax)
+            z = np.random.uniform(self.dmin, self.dmax)
+            r2 = x ** 2 + y ** 2 + z ** 2
+
         r = np.sqrt(r2)
-        if r2 > self.dmax2:
-            r = self.pick_distance()
         return r
